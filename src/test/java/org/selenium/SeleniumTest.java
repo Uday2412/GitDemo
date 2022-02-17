d09129807870cd7a1898e25c1a3a226bcf7bae7c
Uday2412 <009kiran.k@gmail.com>
2022-02-17 12:37:46 +0530

    first commit

---
+++ src/test/java/org/selenium/SeleniumTest.java
@@ -0,0 +1,17 @@
+package org.selenium;
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	public void seleniumMethod1() 
+	{
+		System.out.println("Selenium Method1");
+	}
+	
+	@Test
+	public void seleniumMethod2() 
+	{
+		System.out.println("Selenium Method2");
+	}
+}
---
+++ src/test/java/org/selenium/Test1.java
@@ -0,0 +1,19 @@
+package org.selenium;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void testing1()
+	{
+		System.out.println("Testing 1");
+	}
+	
+	@Test
+	public void testing2()
+	{
+		System.out.println("Testing 2");
+	}
+
+}
